3763c9f37bcbe35748013219cbb325b81826533f
mdtaufiq28 <mohammed.taufiq.n@gmail.com>
2025-05-24 14:51:34 +0530

    added new log message

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import { AccessToken } from 'livekit-server-sdk';
 const app=express();
 const PORT=process.env.PORT || 4000;
 
+console.log("Welcome to Server of Chefira");
 
 app.use(cors());
 app.use(express.json());
